1de1278e7950e7764931ba06da9d5a8d4c17d412
bodybreaker <bodybreaker@naver.com>
2021-10-31 15:52:20 +0900

    update

--- faceRecogServer.py
+++ faceRecogServer.py
@@ -8,6 +8,7 @@ import io
 import numpy as np
 import tensorflow as tf
 from tensorflow import keras
+import json
 import cv2
 
 
@@ -22,25 +23,31 @@ faceModel.summary()
 class_names=['CL (가수)', 'G.NA', '강동원', '강인 (가수)', '고아성', '공유', '구교환', '구하라', '권상우', '김건모', '김고은', '김민정', '김종국 (가수)', '김태희', '김혜수', '닉쿤', '류승범', '박진영', '박해일', '보아', '비 (가수)', '송강호', '송중기', '승리', '아이유', '안성기', '유노윤호', '유이 (배우)', '이병헌', '이승기', '이정재', '이효리', '장나라', '장동건', '전도연', '조성모', '최강창민', '최민식', '케이윌', '하정우', '한석규', '허각', '현아', '황정민', '효린']
 
 def predictFace(imageList):
-
+    
+    resultList=[]
+    
     for image in imageList:
-
-
         image_resized = cv2.resize(image,(100,100),interpolation=cv2.INTER_CUBIC)
-
         img_array = tf.expand_dims(image_resized, 0) # Create a batch
+
         predictions = faceModel.predict(img_array)
         score = tf.nn.softmax(predictions[0])
-
         sortedScore = np.argsort(-score)[0:3]
-        i = 0;
 
+        celebFaceList = []# 최종 3위 얼굴 리스트
 
-
+        i = 0;        
         for rank in sortedScore:
+            face={
+                'name':class_names[rank],
+                'percent': str(round((score[rank]*100).numpy(),2)),
+                'num':str(rank)
+            }
             print("[{}]순위 : {} - {:.2f}%".format(i+1,class_names[rank],score[rank]*100))
+            celebFaceList.append(face)
             i+=1
-
+        resultList.append(celebFaceList)
+    return resultList
 
 @app.route('/')
 def test():
@@ -64,9 +71,11 @@ def face_recog():
         #image.show(Image.fromarray(image_np)) 디버깅용
         faceImageList.append(image_np)
         
-    predictFace(faceImageList)
+    predictList = predictFace(faceImageList)
 
-    return 'test'
+    print(predictList)
+
+    return json.dumps(predictList)
 
 if __name__ == "__main__":
     app.debug = True
